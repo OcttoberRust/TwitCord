c99e12351c8e402ec78e19fa49f36e71dd4c066f
DustyOro <37097866+PJacobo@users.noreply.github.com>
2021-11-15 21:25:53 +0000

    Initial commit

---
+++ discordbot.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import time
+import discord.ext
+from discord.utils import get
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions,  CheckFailure, check
+#^ basic imports for other features of discord.py and python ^
+
+client = discord.Client()
+
+client = commands.Bot(command_prefix = '!') #put your own prefix here
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
+
+
+
+
+@client.event
+async def on_message(message):
+  if message.author == client:
+    return
+
+  msg = message.content
+
+
+client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
+#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
+#enjoy!
---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import time
+import discordbot
+import twitterbot
+
+client = discordbot.discord.Client()
+
+client = discordbot.commands.Bot(command_prefix = '!') #put your own prefix here
+
+
+"""import discord
+import discord.ext
+from discord.utils import get
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions,  CheckFailure, check
+#^ basic imports for other features of discord.py and python ^
+
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
+
+
+
+@client.event
+async def on_message(message):
+  if message.author == client:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  options = starter_encouragements
+  if "encouragements" in db.keys():
+    options = options + db["encouragements"]
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith("$new"):
+    encouraging_message = msg.split("$new ", 1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragement(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+"""
+
+client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
+#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
+#enjoy!
