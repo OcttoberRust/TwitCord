fe4b42243edf3686a0f1cafbb70dba7f18404976
DustyOro <37097866+PJacobo@users.noreply.github.com>
2021-11-17 23:01:11 +0000

    Imported os retrieval for tweepy and discord

--- twitterbot.py
+++ twitterbot.py
@@ -1,10 +1,11 @@
 import tweepy
+import os
 
-consumer_key = ""
-consumer_secret = ""
+consumer_key = os.getenv("TWITTER_API_KEY")
+consumer_secret = os.getenv("TWITTER_API_KEY_SECRET")
 
-access_token = ""
-access_token_secret = ""
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
